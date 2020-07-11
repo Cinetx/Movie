82e24aaf9f3fa7e1656857d003fef5ae89ef5e43
Denis <cinets0@gmail.com>
2020-07-11 03:05:45 +0300

    // выводит сообщение с вопросом о любимом жанре и упорядочивает их в топ 3

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || numberOfFilms == isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 
 let personalMovieDB = {
         count : numberOfFilms,
@@ -12,27 +21,50 @@ let personalMovieDB = {
     };
 
 // Вывод сообщения в зависимости от кол-во просмотренных фильмов
-if (personalMovieDB.count < 10) {
-    alert('Просмотренно довольно мало фильмов');
-    console.log('2')
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-    console.log('1')
-} else if (personalMovieDB.count >= 50) {
-    alert('Вы киноман');
-} else {
-    console.log('Произошла ошибка')
-}
-
-// Задает вопросы, ограничение в вводе символов или пустой строки
-for (let i = 0; i < 2; i++) {
-    let question1 = prompt('Один из последних просмотренных фильмов?', ''),
-        question2 = prompt('На сколько оцените его?', '');
-    
-    if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50 && question2.length < 50) {
-        personalMovieDB.movies[question1] = question2;
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотренно довольно мало фильмов');
+        console.log('2')
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Вы классический зритель');
+        console.log('1')
+    } else if (personalMovieDB.count >= 50) {
+        alert('Вы киноман');
     } else {
-        i--;
+        console.log('Произошла ошибка')
+    }
+}
+detectPersonalLevel()
+
+// Задает вопросы о последних фильмах, ограничение в вводе символов или пустой строки
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let question1 = prompt('Один из последних просмотренных фильмов?', ''),
+            question2 = prompt('На сколько оцените его?', '');
+        
+        if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50 && question2.length < 50) {
+            personalMovieDB.movies[question1] = question2;
+        } else {
+            i--;
+        }
     }
 }
 
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
+
+// выводит сообщение с вопросом о любимом жанре и упорядочивает их в топ 3
+function writeYourGenres() {
+    for( let i = 1; i <= 3; i++) {
+        let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+writeYourGenres()
+
+console.log(personalMovieDB.genres);
